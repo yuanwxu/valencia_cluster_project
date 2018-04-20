5618ea25309f6dff7a82af1ff3c60190f7648fe7
Yuanwei Xu <yuanwxu@126.com>
2018-04-20 12:42:51 +0100

    Fix zero branch lengths of timed trees by shifting *all* branch lengths by 0.1
    Add TransPhylo run to the pipeline.
    
    - Sharing params across all clusters
    
    - Divide clusters into two groups such that the edge length scale
    within each group are approx equal, sharing params within groups.

--- valencia_clusters_final.R
+++ valencia_clusters_final.R
@@ -4,6 +4,7 @@
 library(ape)
 library(tidyverse)
 library(treedater)
+library(TransPhylo)
 
 # Function to read ML trees corresponding to the clusters
 # Here these are built from raxml
@@ -54,3 +55,66 @@ cls_sts <- map2(cls_mltree_unrooted, cls_name, get_sts2Treedater)
 # Make time-trees from treedater
 # Source tdaterAnalysis from analysis4.Rmd
 cls_timetree <- tdaterAnalysis(cls_mltree_unrooted, cls_sts, 1, strictClock = TRUE, meanRateLimits = c(0.3, 0.7))
+
+# Fix zero branch lengths of timed trees by shifting *all* branch lengths by 0.1
+fix_edge_length <- function(timed_tree){
+  if(any(dplyr::near(timed_tree$edge.length,0))){
+    timed_tree$edge.length <- timed_tree$edge.length + 0.1
+    return(timed_tree)
+  }
+  else # do nothing
+    return(timed_tree)
+}
+cls_timetree <- map(cls_timetree, fix_edge_length)
+
+# Run TransPhylo
+cls_lastdate <- map(cls_timetree, ~ max(.$sts))
+cls_ptree <- map2(cls_timetree, cls_lastdate, ptreeFromPhylo)
+source("~/Biomath/TransPhylo/R/proposal.R")
+source("~/Biomath/TransPhylo/R/computeHost.R")
+source("~/Biomath/TransPhylo/R/infer_multiTTree_shareParam.R")
+
+ws.shape <- 1.1; ws.scale <- 1/0.4
+w.shape <- 1.3; w.scale <- 1/0.3
+iters <- 2e4; thin <- 10
+set.seed(1)
+cls_record <- infer_multiTTree_shareParam(cls_ptree, w.shape, w.scale, ws.shape, ws.scale, 
+                                         mcmcIterations = iters, thinning = thin, 
+                                         share = c("neg","off.r","off.p","pi"))
+# Simple trace plot of shared parameters
+plot_trace <- function(record){
+  par(mfrow = c(1,3))
+  plot(map_dbl(record, "neg"), type = "l", ylab = "neg", xlab = "MCMC step")
+  plot(map_dbl(record, "off.r"), type = "l", ylab = "off.r", xlab = "MCMC step")
+  plot(map_dbl(record, "pi"), type = "l", ylab = "pi", xlab = "MCMC step")  
+}
+plot_trace(cls_record[[1]])
+
+discard_burnin <- function(record_lst, burnin = 0.2){
+  map(record_lst, function(x) x[round(length(x)*burnin):length(x)])
+}
+cls_record_small <- discard_burnin(cls_record)
+rm(cls_record)
+
+# Share params within two groups defined by edge length scale
+g1 <- c(2, 5, 10, 16, 17, 21) # scale = 0.5
+g2 <- setdiff(1:20, g1) # scale >= 1
+cls_record_g1 <- infer_multiTTree_shareParam(cls_ptree[g1], w.shape, w.scale, ws.shape, ws.scale, 
+                                          mcmcIterations = iters, thinning = thin, 
+                                          share = c("neg","off.r","off.p","pi"))
+cls_record_g2 <- infer_multiTTree_shareParam(cls_ptree[g2], w.shape, w.scale, ws.shape, ws.scale, 
+                                          mcmcIterations = iters, thinning = thin, 
+                                          share = c("neg","off.r","off.p","pi"))
+names(cls_record_g1) <- cls_name[g1]
+names(cls_record_g2) <- cls_name[g2] # for easy referencing
+plot_trace(cls_record_g1[[1]])
+plot_trace(cls_record_g2[[1]])
+
+## Observation: g1 shows higher sampling rate pi than g2, and lower neg; whereas sharing params for all clusters 
+#               averages out these effects
+
+# Check transmission tree for cluster 77
+source("~/Biomath/TransPhylo/R/selectTTree/selectTTree.R")
+whichtree <- selectTTree(cls_record_g2[["CL077"]], burnin = 0.2)
+plotCTree(cls_record_g2[["CL077"]][[whichtree]]$ctree)
+plotTTree2(extractTTree(cls_record_g2[["CL077"]][[whichtree]]$ctree))
